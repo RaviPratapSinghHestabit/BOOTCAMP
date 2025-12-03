312027df824a27bce3975aa9883335ebd3e132a6
Ravi Pratap Singh <ravis.hestabit@gmail.com>
2025-12-03 19:44:10 +0530

    Delete bootcamp_week4/bootcamp_day1 directory

--- bootcamp_week4/bootcamp_day1/src/routes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-
-router.use("/users", require("./user.routes"));
-router.use("/products", require("./product.routes"));
-
-module.exports = router;
--- bootcamp_week4/bootcamp_day1/test-db.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config({ path: '.env.local' });
-const mongoose = require('mongoose');
-
-console.log("MONGO_URI:", process.env.MONGO_URI);
-
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("✅ MongoDB connected"))
-.catch(err => console.error("❌ MongoDB connection failed:", err));
