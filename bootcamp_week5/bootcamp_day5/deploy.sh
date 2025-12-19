c084bbe6b33e8d707f528d22f66b9b364b15af4e
Ravi Pratap Singh <ravis.hestabit@gmail.com>
2025-12-19 19:21:50 +0530

    commiting some week5 and week6 tasks

---
+++ bootcamp_week5/bootcamp_day5/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+echo "🚀 Starting Deployment..."
+
+echo "🔧 Building and starting containers..."
+docker compose -f docker-compose.prod.yml up -d --build
+
+echo "📦 Running containers:"
+docker ps
+
+echo "📜 Tailing logs..."
+docker compose -f docker-compose.prod.yml logs -f
